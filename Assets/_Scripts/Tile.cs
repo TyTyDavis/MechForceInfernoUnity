0ba98402bffe5272c03562e55cc9590f3d6f5ce1
Tyler Davis <tylerrusselldavis@gmail.com>
2022-06-08 21:31:02 -0500

    continuing basic grid setup

--- Assets/_Scripts/Tile.cs
+++ Assets/_Scripts/Tile.cs
@@ -7,9 +7,18 @@ public class Tile : MonoBehaviour
 
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
-    // Start is called before the first frame update
+    [SerializeField] private GameObject _highlight;
+
     public void Init(bool isOffset){
         _renderer.color = isOffset ? _offsetColor : _baseColor;
 
+    }
+
+    void OnMouseEnter(){
+        _highlight.SetActive(true);
+    }
+
+    void OnMouseExit(){
+        _highlight.SetActive(false);
     }
 }
